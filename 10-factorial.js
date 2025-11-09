b06fc842a226faa768902626edda583ee6be6450
Nasiru Bawa <haskenasir8@gmail.com>
2025-11-09 13:04:33 +0000

    factorial

---
+++ 10-factorial.js
@@ -0,0 +1,10 @@
+function factorial(n) {
+  if (n <= 1) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+}
+
+const input = Number(process.argv[2]);
+
+console.log(factorial(isNaN(input) ? 1 : input));
