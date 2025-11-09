bdc0dc68a86a7519348387e5485159cd67ba2670
Nasiru Bawa <haskenasir8@gmail.com>
2025-11-09 12:22:08 +0000

    arguments passed concat using is

--- 4-concat.js
+++ 4-concat.js
@@ -1,7 +1,3 @@
 const args = process.argv.slice(2);
 
-if (args[0] !== undefined && args[1] !== undefined) {
-  console.log(`${args[0]} is ${args[1]}`);
-} else {
-  console.log("Missing arguments");
-}
+console.log(`${args[0]} is ${args[1]}`);
